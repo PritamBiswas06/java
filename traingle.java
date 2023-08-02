f3a5c5f30d0c1789058eb107d24e54926002b760
PritamBiswas06 <126695473+PritamBiswas06@users.noreply.github.com>
2023-08-02 23:35:39 +0530

    traingle

--- traingle.java
+++ traingle.java
@@ -1,5 +1,20 @@
 public class traingle {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static void mytraingle(Object[] arr) {
+        for (int row=0;row<arr.length;row++) {
+            for(int col=0;col<row;col++){
+                System.out.print(arr[col]);
+
+            }
+            System.out.println();
+            
+        }
+        
+    }
+    public static void main(String args[]) {
+        // System.out.println("Hello, World!");
+        Integer[] arr = new Integer[]{10,20,30,40,50,60};
+        // mytraingle(new Integer[] {1,2,3,4,5});
+        mytraingle(arr);
     }
 }
